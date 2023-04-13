7697c60dcefc0dd2cfcf86a2a58bf30b43d96ddc
Pedro Arriola <pedroparty310@gmail.com>
2023-04-12 19:55:12 -0600

    Cambios en graph

--- main.py
+++ main.py
@@ -9,50 +9,42 @@
 
 from py2neo import Graph, Node, Relationship
 
-graph = Graph("neo4j+s://f4c961e7.databases.neo4j.io", auth=("neo4j", "rv9qlnIH1-vO9YEuuFQMpJEjz1aMSLcqNveDWxMMKVg"))
+graph = Graph("neo4j+s://4191abc9.databases.neo4j.io", auth=("neo4j", "kfoop9SX8490ifTOARQKazVVTJ_4iAcIJS7zD8lk6_0"))
 
-def create_user(name):
-    return Node("User", name=name)
 
-def create_movie(title):
-    return Node("Movie", title=title)
-
-def create_relationship(user, movie, rating):
-    return Relationship(user, "RATED", movie, rating=rating)
-
-def add_user_to_graph(graph, name):
-    user = create_user(name)
+def add_user(graph, name, userID):
+    user = Node("User", name=name, userID=userID)
     graph.create(user)
 
-def add_movie_to_graph(graph, title):
-    movie = create_movie(title)
+def add_movie(graph, title, movieID, year, plot):
+    movie = Node("Movie", title=title, movieID=movieID, year=year, plot=plot)
     graph.create(movie)
 
-def add_rating_to_graph(graph, user_name, movie_title, rating):
-    user = create_user(user_name)
-    movie = create_movie(movie_title)
-    rating_rel = create_relationship(user, movie, rating)
-    graph.create(rating_rel)
+def add_rating(graph, user_id, movie_id, rating, timestamp):
+    user = Node("User", userID=user_id)
+    movie = Node("Movie", movieID=movie_id)
+    rated = Relationship(user, "RATED", movie, rating=rating, timestamp=timestamp)
+    graph.create(rated)
 
-# Add some users, movies, and ratings to the graph
-add_user_to_graph(graph, "Alice")
-add_user_to_graph(graph, "Bob")
-add_user_to_graph(graph, "Charlie")
-add_user_to_graph(graph, "Dave")
-add_user_to_graph(graph, "Eve")
+# Se agregan usuarios, peliculas y sus respectivos ratings a la instancia de Neo4j
+add_user(graph, "Alice", "1")
+add_user(graph, "Bob", "2")
+add_user(graph, "Charlie", "3")
+add_user(graph, "Dave", "4")
+add_user(graph, "Eve", "5")
 
-add_movie_to_graph(graph, "The Matrix")
-add_movie_to_graph(graph, "Inception")
-add_movie_to_graph(graph, "The Godfather")
-add_movie_to_graph(graph, "The Shawshank Redemption")
-add_movie_to_graph(graph, "Pulp Fiction")
+add_movie(graph, "The Matrix", 1, 1999, "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.")
+add_movie(graph, "Inception", 2, 2010, "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.")
+add_movie(graph, "The Godfather", 3, 1972, "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.")
+add_movie(graph, "The Shawshank Redemption", 4, 1994, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.")
+add_movie(graph, "Pulp Fiction", 5, 1994, "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.")
 
-add_rating_to_graph(graph, "Alice", "The Matrix", 4)
-add_rating_to_graph(graph, "Alice", "Inception", 5)
-add_rating_to_graph(graph, "Bob", "The Godfather", 5)
-add_rating_to_graph(graph, "Bob", "The Shawshank Redemption", 4)
-add_rating_to_graph(graph, "Charlie", "The Matrix", 3)
-add_rating_to_graph(graph, "Charlie", "The Shawshank Redemption", 5)
-add_rating_to_graph(graph, "Dave", "Pulp Fiction", 4)
-add_rating_to_graph(graph, "Eve", "Inception", 4)
-add_rating_to_graph(graph, "Eve", "The Godfather", 3)
+add_rating(graph, "1", 1, 4, 1649862000)
+add_rating(graph, "1", 2, 5, 1649862000)
+add_rating(graph, "2", 3, 5, 1649862000)
+add_rating(graph, "2", 4, 4, 1649862000)
+add_rating(graph, "3", 1, 3, 1649862000)
+add_rating(graph, "3", 4, 5, 1649862000)
+add_rating(graph, "4", 5, 4, 1649862000)
+add_rating(graph, "5", 2, 4, 1649862000)
+add_rating(graph, "5", 3, 3, 1649862000)
